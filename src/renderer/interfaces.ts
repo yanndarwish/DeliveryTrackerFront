c40844afe7a1fa1cf76f7fdb9775d633ab9edb46
Yann Darwish <yann.darwish@gmail.com>
2023-05-07 14:00:58 +0200

    interface

--- src/renderer/interfaces.ts
+++ src/renderer/interfaces.ts
@@ -1,6 +1,10 @@
-export interface User {
-  _id: String;
+export interface PostUser {
   name: String;
+}
+
+export interface User extends PostUser {
+  _id: String;
+  __v: Number;
 }
 
 export interface Link {
