dd5b7bb577fdf8aa5928c918f456aec24bce668c
Yann Darwish <yann.darwish@gmail.com>
2023-05-19 13:57:31 +0200

    feature: client table and sidePanel

---
+++ src/renderer/components/globals/Table.tsx
@@ -0,0 +1,69 @@
+import { MdCheck, MdClose } from 'react-icons/md';
+
+export interface ITableProps {
+  header: { title: string; field: string[] }[];
+  data: any[];
+}
+
+const Table = (props: ITableProps) => {
+  return (
+    <div className="relative flex flex-col overflow-x-auto pt-6">
+      <div className="inline-block min-w-full py-2">
+        <div className="overflow-x-auto">
+          <table className="min-w-full text-left text-sm font-light">
+            {/* Table Header */}
+            <thead className="border-b font-medium dark:border-neutral-500">
+              <tr>
+                <th scope="col" className="px-4 py-4">
+                  N°
+                </th>
+                {props.header.map((item, index) => (
+                  <th scope="col" className="px-4 py-4" key={index}>
+                    {item.title}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            {/* Table Body */}
+            <tbody>
+              {props.data.map((row, i) => (
+                <tr
+                  key={row._id}
+                  data-id={row._id}
+                  className="border-b dark:border-neutral-500"
+                >
+                  <td className="whitespace-nowrap px-4 py-4 font-medium">
+                    {i + 1}
+                  </td>
+                  {props.header.map((item, i) => (
+                    <td
+                      key={i}
+                      className={`whitespace-nowrap px-4 py-4 font-medium ${
+                        i === 0 ? 'uppercase' : 'capitalize'
+                      } ${row[item.field[0]] === true ? "text-emerald-500" : row[item.field[0]] === false && "text-red-500"}`}
+                    >
+                      {item.field.length > 1 && row[item.field[0]] !== null ? (
+                        row[item.field[0]] + ' ' + row[item.field[1]]
+                      ) : item.field.length > 1 &&
+                        row[item.field[0]] === null ? (
+                        row[item.field[1]]
+                      ) : row[item.field[0]] === true ? (
+                        <MdCheck size={24} />
+                      ) : row[item.field[0]] === false ? (
+                        <MdClose size={24} />
+                      ) : (
+                        row[item.field[0]]
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
--- src/renderer/pages/Clients.tsx
+++ src/renderer/pages/Clients.tsx
@@ -1,100 +1,72 @@
 // import { useGetClientsQuery } from 'renderer/redux/services/clients';
 import { clients } from 'renderer/mock';
-import { MdCheck, MdClose } from 'react-icons/md';
+import { IoMdAdd } from 'react-icons/io';
+import { useAppSelector } from 'renderer/redux/hooks';
+import SidePanel from 'renderer/components/globals/SidePanel';
+import { useState } from 'react';
+import CreateClientForm from 'renderer/components/CreateClientForm';
+import Table from 'renderer/components/globals/Table';
 
 export interface IClientsProps {}
 
 const Clients = (props: IClientsProps) => {
+  const [open, setOpen] = useState<Boolean>(false);
   // const { data } = useGetClientsQuery(null);
   // console.log(data);
+  const user = useAppSelector((state) => state.user.user);
+
+  const formatedData = {
+    header: [
+      { title: 'Nom', field: ['name'] },
+      { title: 'Adresse', field: ['streetNumber', 'streetName'] },
+      { title: 'Ville', field: ['city'] },
+      { title: 'Code Postal', field: ['postalCode'] },
+      { title: 'Pays', field: ['country'] },
+      { title: 'Actif', field: ['active'] },
+    ],
+    data: clients,
+  };
+
+  console.log(clients);
   return (
     <div className="relative h-full px-6 text-white">
-      {/* page title */}
-      <h1 className="text-4xl font-medium">Clients</h1>
+      {/* page header */}
+      <div className="flex justify-between items-center pr-2">
+        <h1 className="text-4xl font-medium">Clients</h1>
+        {clients.filter((client) => client.createdBy === user._id).length ? (
+          <button
+            className="p-1 rounded-full text-emerald-500 border border-emerald-500 focus:outline-emerald-500 hover:text-emerald-500 focus:text-emerald-500  hover:bg-neutral-700 focus:bg-neutral-700"
+            onClick={() => setOpen(true)}
+            autoFocus
+          >
+            <IoMdAdd size={24} />
+          </button>
+        ) : (
+          ''
+        )}
+      </div>
+      {/* filter */}
+
       {/* table */}
-      {clients.length ? (
-        <div className="relative flex flex-col overflow-x-auto pt-6">
-          <div className="">
-            <div className="inline-block min-w-full py-2">
-              <div className="overflow-x-auto">
-                <table className="min-w-full text-left text-sm font-light">
-                  <thead className="border-b font-medium dark:border-neutral-500">
-                    <tr>
-                      <th scope="col" className="px-4 py-4">
-                        N°
-                      </th>
-                      <th scope="col" className="px-4 py-4">
-                        Nom
-                      </th>
-                      <th scope="col" className="px-4 py-4">
-                        Adresse
-                      </th>
-                      <th scope="col" className="px-4 py-4">
-                        Ville
-                      </th>
-                      <th scope="col" className="px-4 py-4">
-                        Code Postal
-                      </th>
-                      <th scope="col" className="px-4 py-4">
-                        Pays
-                      </th>
-                      <th scope="col" className="px-4 py-4">
-                        Actif
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {clients.map((client, i) => (
-                      <tr
-                        key={client._id}
-                        data-id={client._id}
-                        className="border-b dark:border-neutral-500"
-                      >
-                        <td className="whitespace-nowrap px-4 py-4 font-medium">
-                          {i + 1}
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-4 uppercase">
-                          {client.name}
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-4 capitalize">
-                          {client.streetNumber === 0
-                            ? client.streetName
-                            : client.streetNumber + ' ' + client.streetName}
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-4 capitalize">
-                          {client.city}
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-4">
-                          {client.postalCode}
-                        </td>
-                        <td className="whitespace-nowrap px-4 py-4 uppercase">
-                          {client.country}
-                        </td>
-                        <td
-                          className={`whitespace-nowrap px-4 py-4 ${
-                            client.active ? 'text-emerald-500' : 'bg-red-500'
-                          }`}
-                        >
-                          {client.active === true ? (
-                            <MdCheck size={24} />
-                          ) : (
-                            <MdClose size={24} />
-                          )}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
+      {clients.filter((client) => client.createdBy === user._id).length ? (
+        <Table header={formatedData.header} data={formatedData.data} />
       ) : (
-        <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-          <p className="text-center">
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center flex-col gap-8">
+          <p className="text-center text-xl">
             Vous n'avez aucun client pour le moment, ajoutez-en un nouveau !
           </p>
+          <button
+            className="p-5 rounded-full text-emerald-500 border border-emerald-500 focus:outline-emerald-500 hover:text-emerald-500 focus:text-emerald-500 bg-neutral-800/50 hover:bg-neutral-700/50 focus:bg-neutral-700/50"
+            onClick={() => setOpen(true)}
+          >
+            <IoMdAdd size={24} />
+          </button>
         </div>
+      )}
+      {open && (
+        <SidePanel title="Nouveau Client" open={open} setOpen={setOpen}>
+          <CreateClientForm setOpen={setOpen} />
+        </SidePanel>
       )}
     </div>
   );
