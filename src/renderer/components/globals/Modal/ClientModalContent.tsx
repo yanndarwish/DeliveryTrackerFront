3d015e1262dcba3b76ba2e850889251ce5ef7955
Yann Darwish <yann.darwish@gmail.com>
2023-06-07 10:40:53 +0200

    feature: client modal group data

--- src/renderer/components/globals/Modal/ClientModalContent.tsx
+++ src/renderer/components/globals/Modal/ClientModalContent.tsx
@@ -1,4 +1,5 @@
 import { Client } from 'renderer/interfaces';
+import { groups } from 'renderer/mock';
 
 export interface IClientModalContentProps {
   data: Client | null;
@@ -14,15 +15,27 @@ const ClientModalContent = (props: IClientModalContentProps) => {
       >
         {props.data?.active ? 'Actif' : 'Inactif'}
       </div>
-      <div>
-        <label htmlFor="" className="text-base font-semibold">
-          Adresse
-        </label>
-        <p className="text-base capitalize">{`${
-          props.data?.streetNumber !== null ? props.data?.streetNumber : ''
-        } ${props.data?.streetName}`}</p>
-        <p className="text-base capitalize">{`${props.data?.postalCode}, ${props.data?.city}`}</p>
-        <p className="text-base capitalize">{props.data?.country}</p>
+      <div className="flex flex-col gap-2">
+          {groups.find((c) => c._id === props.data?.group[0]) && 
+        <div>
+          <label htmlFor="" className="text-base font-semibold">
+            Groupe
+          </label>
+          <p className="text-base capitalize">
+            {groups.find((c) => c._id === props.data?.group[0])?.name}
+          </p>
+        </div>
+        }
+        <div>
+          <label htmlFor="" className="text-base font-semibold">
+            Adresse
+          </label>
+          <p className="text-base capitalize">{`${
+            props.data?.streetNumber !== null ? props.data?.streetNumber : ''
+          } ${props.data?.streetName}`}</p>
+          <p className="text-base capitalize">{`${props.data?.postalCode}, ${props.data?.city}`}</p>
+          <p className="text-base capitalize">{props.data?.country}</p>
+        </div>
       </div>
     </div>
   );
